5261c9600aff37ca186646a4b58683689280e9e3
s28222 <s28222@pjwstk.edu.pl>
2024-04-14 18:00:46 +0200

    Version where example2 does not fail but code has problems, it is not so accurate

---
+++ src/DataPoint.java
@@ -0,0 +1,9 @@
+public class DataPoint {
+    double[] inputs;
+    int label;
+
+    public DataPoint(double[] inputs, int label) {
+        this.inputs = inputs;
+        this.label = label;
+    }
+}
